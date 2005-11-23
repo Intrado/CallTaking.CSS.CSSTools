516d0c7497f4e57a576182eca388000abdf80933
sbeliveau <a@b>
2005-11-23 16:40:32 -0500

    Update prepare.rb
    [git-p4: depot-paths = "//call_taking/CSS/CSSTools/main/": change = 10405]

--- prepare.rb
+++ prepare.rb
@@ -1,34 +1,16 @@
-require 'FileUtils'
-require 'P4'
+require 'p4'
+
+p4 = P4.new()
+p4.parse_forms
+p4.connect()
+client = p4.fetch_client()
+p4.run_sync("-f", "//backroom/buildtools/...")
+require client['Root']+'/backroom/buildtools/buildtools'
+p4.disconnect
 
 $releaseDepot = "releases"
 
 $p4Deps = ["positron/common libraries/diag/2.0.1.18/DiagExp"]
-
-def getP4Export(project, p4, clientRoot)
-  fullName = $releaseDepot + "/" + project
-  print "Importing " + fullName + "\n"
-  p4.run_sync("-f", "//" + fullName + "/...")
-  projectName = project.split("/")[-3]
-  FileUtils.cp_r(clientRoot + "/" + fullName, "_import" + "/" +  projectName)
-end
-
-def getP4Exports()
-  p4 = P4.new()
-  p4.parse_forms
-  p4.connect()
-  clientRoot = p4.fetch_client()["Root"]
-  begin
-    $p4Deps.each { |dep| getP4Export(dep, p4, clientRoot)}
-  rescue P4Exception => msg
-    puts( msg )
-    p4.warnings.each { |w| puts( w ) }
-    p4.errors.each { |e| puts( e ) }
-  ensure
-    puts(p4.output)
-    p4.disconnect
-  end
-end 
 
 # Main
 print "Importing Project Dependencies...\n"
