253846e9f50c1983a264d8904e3ee041ce587cf1
Stéphane Kouassi <skouassi@positron911.com>
2012-10-30 14:23:05 -0500

    Get the Servicepack in "HKLM\\Software\\Positron\\Pots\\Servicepacks"
    [git-p4: depot-paths = "//call_taking/CSS/CSSTools/main/": change = 184871]

--- ViperTools.cpp
+++ ViperTools.cpp
@@ -2,6 +2,7 @@
 #include "ViperTools.h"
 #include "Diag.h"
 #include <sstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -67,7 +68,42 @@ int cViperTools::GetViperServicePack(std::string diagModuleName)
     {
       sp = GetRegistryKey(diagModuleName, "Software\\Positron Industries Inc.\\Viper", "SP");
     }
-  
+    if (sp.size() == 0)
+    {
+      string version = GetRegistryKey(diagModuleName, "Software\\Positron\\Pots", "Version");
+
+      // We will try to determine the most recent sp applied by checking in the registery if this sp was applied
+      bool checkfornewersp = true;
+      int count = 0;
+      do
+      {
+         // Create the key to look for(for example 4.1.0.10-sp01 for service pack 1)
+         ostringstream ost;
+         ost << version << "-sp" << std::setw(2) << std::setfill('0') << count+1;
+         string vsp = ost.str();
+
+         // Try to get the value of this key
+         string val = GetRegistryKey(diagModuleName, "Software\\Positron\\Pots\\Servicepacks", vsp);
+         if (val.size() > 0 && val.find("Applied")!=string::npos)
+         {
+           // This key is in the registry. It means that this sp was applied, continue to check if a newer one was applied
+           count++;
+         }
+         else
+         {  
+           // This key is not in the registry. It means that this sp was not applied, do not continue to check for newer sp
+           // The latest sp applied is the one we are looking for 
+           checkfornewersp = false;            
+         }
+      }
+      while(checkfornewersp);
+
+      ostringstream ost;
+      ost.str("");
+      ost << count;
+      sp = ost.str();
+    }
+
     if (sp.size() > 0)
     {
       istringstream iss;
