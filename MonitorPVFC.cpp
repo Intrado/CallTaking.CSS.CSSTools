64a6108605a79c3d59cad17f324b75f90fbb3550
Eric April <Eric.April@intrado.com>
2016-11-08 12:49:31 -0500

    VIPER-9174: Stop WerFault.exe if running prior to exiting in a detected pure virtual function call run-time error.
    [git-p4: depot-paths = "//call_taking/CSS/CSSTools/main/": change = 251858]

--- MonitorPVFC.cpp
+++ MonitorPVFC.cpp
@@ -3,6 +3,8 @@
 #include "cEvent.h"
 #include "Diag.h"
 #include "windows.h"
+#include <Tlhelp32.h>
+
 
 #define kModuleName "MonitorPVFC"
 
@@ -13,6 +15,7 @@ public:
   virtual long ThreadProc();
   void Signal(){mWaitEvent.Signal();}
   bool CheckForPVFC();
+  void StopWerfaultIfRunning();
 
   int mFrequencyInSeconds;
   bool mTerminate;
@@ -64,6 +67,8 @@ long cMonitorPVFCThread::ThreadProc()
   {
     if (CheckForPVFC())
     {
+      Sleep(1000); // give some time to WerFault to show up before checking if running to stop it
+      StopWerfaultIfRunning(); 
       DiagError(kModuleName, "cMonitorPVFCThread", "Pure Virtual Function Call detected. Exiting process right away");
       ExitProcess(0);
     }
@@ -82,4 +87,37 @@ bool cMonitorPVFCThread::CheckForPVFC()
     return true;
   }
   return false;
+}
+
+void cMonitorPVFCThread::StopWerfaultIfRunning()
+{
+  // check if a POTS - only kill WerFault.exe if a POTS
+  DWORD ftyp = GetFileAttributesA("p:\\programs\\ppmsservice");
+  if (!(ftyp & FILE_ATTRIBUTE_DIRECTORY))
+  {
+    //PPMSDiag.SendWarning("Process::StopWerfaultIfRunning()", "Not a POTS server.");
+    return;
+  }
+
+  const char* filename = "WerFault.exe";
+  HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+  PROCESSENTRY32 pEntry;
+  pEntry.dwSize = sizeof (pEntry);
+  BOOL hRes = Process32First(hSnapShot, &pEntry);
+  while (hRes)
+  {
+    if (stricmp(pEntry.szExeFile, filename) == 0)
+    {
+      DiagWarning(kModuleName, "cMonitorPVFCThread::StopWerfaultIfRunning()", "Found a match for WerFault.exe process.");
+      HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, 0, (DWORD) pEntry.th32ProcessID);
+      if (hProcess != NULL)
+      {
+        DiagWarning(kModuleName, "cMonitorPVFCThread::StopWerfaultIfRunning()", "Killing WerFault.exe process.");
+        TerminateProcess(hProcess, 0);
+        CloseHandle(hProcess);
+      }
+    }
+    hRes = Process32Next(hSnapShot, &pEntry);
+  }
+  CloseHandle(hSnapShot);
 }
